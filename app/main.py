296435dd219e4a179a3363c7a9fe9cfcc3ad5447
Hamza Zubair <152253384+mhamza062@users.noreply.github.com>
2026-06-23 01:22:08 +0500

    Implement fetch endpoints for users, assets, and nominees
    Added endpoints to fetch users, assets, and nominees from Firebase.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,38 @@
 from fastapi import FastAPI
+from app.firebase import db
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {"message": "E-Waris Backend Running Successfully"}
+
+
+# Generic function (reuse logic)
+def fetch_collection(collection_name: str):
+    docs = db.collection(collection_name).stream()
+
+    results = []
+
+    for doc in docs:
+        data = doc.to_dict()
+        data["id"] = doc.id
+        results.append(data)
+
+    return results
+
+
+@app.get("/users")
+def get_users():
+    return fetch_collection("Users")
+
+
+@app.get("/assets")
+def get_assets():
+    return fetch_collection("Assets")
+
+
+@app.get("/nominees")
+def get_nominees():
+    return fetch_collection("Nominees")
